5764ff00fd6a474c66c3aa921fbaae51da518d5a
ConstDet <constx7@gmail.com>
2023-02-16 21:55:09 +0300

    Добавлен класс MagicBox.java

---
+++ src/MagicBox.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MagicBox<T> {
+    int quantity;//храним максимальное число элементов коробки
+    T[] arrayItem;//сама коробка
+    public MagicBox(int quantity) {
+        arrayItem = (T[]) new Object[quantity];
+    }
+
+    public boolean add(T item) {
+        for (int i = 0; i < arrayItem.length; i++) {
+            if (arrayItem[i] == null) {
+                arrayItem[i] = item;
+                return true;
+            }
+        }
+        return false;//коробка полна, сообщаем и выходим
+    }
+
+    public T pick() {
+        int count = 0;
+        for (int i = 0; i < arrayItem.length; i++) {
+            if (arrayItem[i] == null) {
+                count++;
+            }
+        }
+        if (count != 0) {
+            throw new RuntimeException("Волшебная коробка не заполнена. Осталось мест: " + count);
+        }
+        Random random = new Random();
+        int randomInt = random.nextInt(quantity + 1);
+        return arrayItem[randomInt];
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int maxQuantity = 4;
+        //стринги
+        MagicBox<String> magicBox = new MagicBox<>(maxQuantity);
+        magicBox.add("Petya");
+        try {
+            String rezString = magicBox.pick();//ошибка
+        } catch (RuntimeException e) {
+            System.out.println("Ошибка заполнения коробки: " + e);
+        }
+        for (int i = 0; i < maxQuantity; i++){
+            if (!magicBox.add("Sasha " + i)) {
+                break;
+            }
+        }
+        System.out.println(magicBox.pick());
     }
 }
